6bd08b3fa479f47da35287693bc2db7c6fcdf27e
dudysz <dudasenamoraes16@gmail.com>
2025-01-31 12:17:10 -0300

    Javascript

---
+++ Javascript/Aula-01/script.js
@@ -0,0 +1,23 @@
+//console.log("Ola Mundo");
+
+//Exemplo de alert
+    //alert("Ola Mundo!");
+
+//Exemplo confirm
+    //var teste = confirm("Ola Mundo! 1")
+    //Exibindo a opcao escolha pelo usuário
+    //console.log("Ola Mundo! 2")  
+
+//Exemplo de prompt
+    //var testePrompt =  prompt("Digite seu nome")
+    //Exibindo o nome de usuario 
+    //console.log(testePrompt)
+    //alert(testePrompt)
+
+//Tentativa de adição
+    var numero1 = Number.parseInt(prompt("Digite o primeiro numero"))
+    var numero2 = Number.parseInt(prompt("Digite o segundo numero"))
+    typeof(typeof(numero1))
+    console.log(typeof(numero1))
+    alert(numero1 + numero2)
+
